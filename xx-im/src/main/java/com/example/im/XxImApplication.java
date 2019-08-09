7939a9ca6dd2526a18dedccfbc12c95b03df3488
eddie <931305033@qq.com>
2019-08-09 10:35:59 +0800

    update: init module

---
+++ xx-im/src/main/java/com/example/im/XxImApplication.java
@@ -0,0 +1,13 @@
+package com.example.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxImApplication.class, args);
+    }
+
+}
---
+++ xx-user/src/main/java/com/example/user/XxUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxUserApplication.class, args);
+	}
+
+}
